388191dcaa8956f4e910333aedf7b2d090285bc8
Mohamed Emad <m.emad550@gmail.com>
2019-04-16 00:54:15 +0200

    add project starter

---
+++ main.js
@@ -0,0 +1,69 @@
+function Upload() {
+    //Reference the FileUpload element.
+    var fileUpload = document.getElementById("fileUpload");
+
+    //Validate whether File is valid Excel file.
+    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
+    if (regex.test(fileUpload.value.toLowerCase())) {
+        if (typeof (FileReader) != "undefined") {
+            var reader = new FileReader();
+
+            //For Browsers other than IE.
+            if (reader.readAsBinaryString) {
+                reader.onload = function (e) {
+                    ProcessExcel(e.target.result);
+                };
+                reader.readAsBinaryString(fileUpload.files[0]);
+            } else {
+                //For IE Browser.
+                reader.onload = function (e) {
+                    var data = "";
+                    var bytes = new Uint8Array(e.target.result);
+                    for (var i = 0; i < bytes.byteLength; i++) {
+                        data += String.fromCharCode(bytes[i]);
+                    }
+                    ProcessExcel(data);
+                };
+                reader.readAsArrayBuffer(fileUpload.files[0]);
+            }
+        } else {
+            alert("This browser does not support HTML5.");
+        }
+    } else {
+        alert("Please upload a valid Excel file.");
+    }
+};
+
+function ProcessExcel(data) {
+    //Read the Excel File data.
+    var workbook = XLSX.read(data, {
+        type: 'binary'
+    });
+
+    //Fetch the name of First Sheet.
+    var firstSheet = workbook.SheetNames[0];
+
+    console.log(firstSheet);
+
+    //Read all rows from First Sheet into an JSON array.
+    var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet]);
+
+    brandNames = []
+    excelRows.forEach(row => {
+        return brandNames.push(row['Brand Name']);
+    });
+    
+    brandNames.forEach(brandName => {
+        if (!brandName.includes('_') && !brandName.includes('.com') && !brandName.includes('.net')) {
+            axios.get(`https://www.facebook.com/`, brandName)
+                .then(data => {
+                    console.log(data);
+                }).catch(error =>
+                    console.log(JSON.stringify(error.config.headers.status))
+                );
+        
+        } else {
+            console.log(` ${brandName} brand name isn't available`)
+        }
+    })
+}
